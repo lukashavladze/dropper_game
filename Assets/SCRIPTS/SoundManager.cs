7e635cfd3347cb254d74fbf43acc74c51330ce4c
LSHAVLADZE <LSHAVLADZE@WS002HP290G9>
2026-05-21 16:05:53 +0400

    sound on/off added

---
+++ Assets/SCRIPTS/SoundManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    public Button soundButton;
+
+    private bool soundEnabled = true;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        soundEnabled = PlayerPrefs.GetInt("SOUND_ON", 1) == 1;
+
+        ApplySound();
+    }
+
+    public void ToggleSound()
+    {
+        soundEnabled = !soundEnabled;
+
+        PlayerPrefs.SetInt("SOUND_ON", soundEnabled ? 1 : 0);
+        PlayerPrefs.Save();
+
+        ApplySound();
+    }
+
+    void ApplySound()
+    {
+        AudioListener.volume = soundEnabled ? 1f : 0f;
+    }
+}
